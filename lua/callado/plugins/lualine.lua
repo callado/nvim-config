95edb7a7e29021e96d98dc9de1e8952d49f1a096
Pedro Callado <pedrocallado@gmail.com>
2023-10-21 19:40:44 +0200

    adds lualine

---
+++ lua/callado/plugins/lualine.lua
@@ -0,0 +1,26 @@
+return {
+	'nvim-lualine/lualine.nvim',
+	requires = { 'nvim-tree/nvim-web-devicons', opt = true },
+	lazy = false,
+	priority = 800,
+	config = function()
+		local theme = require("lualine.themes.rose-pine")
+		local modes = {
+			"normal",
+			"insert",
+			"visual",
+			"replace",
+			"command",
+			"inactive",
+		}
+		for _, mode in ipairs(modes) do
+			theme[mode].c.bg = "NONE"
+		end
+		
+		require('lualine').setup({
+			options = { 
+				theme = theme,
+			}
+		})
+	end,
+}
--- lua/callado/plugins/treesitter.lua
+++ lua/callado/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 local M = {
 	"nvim-treesitter/nvim-treesitter",
 	build = function() 
-  		require("nvim-treesitter.install").update({ with_sync = true })()
+		require("nvim-treesitter.install").update({ with_sync = true })()
 	end,
 	config = function()
 		require("nvim-treesitter.install").compilers = { "clang", "cl", "gcc" }
@@ -29,13 +29,16 @@ local M = {
 				enable = true,
 
 				-- list of language that will be disabled
-			-- 	disable = { "c", "rust" },
+				-- 	disable = { "c", "rust" },
 
 				-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
 				-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
 				-- Using this option may slow down your editor, and you may see some duplicate highlights.
 				-- Instead of true it can also be a list of languages
-			-- 	additional_vim_regex_highlighting = false,
+				additional_vim_regex_highlighting = false,
+			},
+			indent = {
+				enable = true,
 			},
 		}
 		-- vim.opt.runtimepath:append("C:\\Users\\PedroCalladoVersiani\\AppData\\Local\\nvim-data\\lazy\\nvim-treesitter\\parseR")
