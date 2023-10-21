8ec872be706d7249a5bf07f0193383cfe54aee29
Pedro Callado <pedrocallado@gmail.com>
2023-10-21 22:26:03 +0200

    sets tab size

--- lua/callado/remap.lua
+++ lua/callado/remap.lua
@@ -1,2 +1,10 @@
 vim.g.mapleader = " "
 -- vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.opt.smarttab = true
+vim.opt.autoindent = true
+vim.opt.expandtab = true
+vim.opt.shiftround = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = -1 
+vim.opt.shiftwidth = 0
+
