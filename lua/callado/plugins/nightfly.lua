73ef369c15aed40f0451a7fa7de5a439779a6d2e
Pedro Callado <pedrocallado@gmail.com>
2023-10-21 18:18:21 +0200

    color & alpha configs

--- lua/callado/plugins/nightfly.lua
+++ lua/callado/plugins/nightfly.lua
@@ -1,11 +1,10 @@
 return {
-
 	"bluz71/vim-nightfly-colors",
 	name = "nightfly",
+	enabled = false,
 	lazy = false,
 	priority = 1000,
 	config = function()
 		vim.cmd [[colorscheme nightfly]]
 	end,
-
 }
