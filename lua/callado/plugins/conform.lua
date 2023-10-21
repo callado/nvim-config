c77992c60c37edc2ce5f555abf194239d931b27c
Pedro Callado <pedrocallado@gmail.com>
2023-10-22 00:26:47 +0200

    adds formatter (conform)

---
+++ lua/callado/plugins/conform.lua
@@ -0,0 +1,16 @@
+return {
+	"stevearc/conform.nvim",
+	opts = {},
+	config = function()
+		require("conform").setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+			},
+			format_on_save = {
+				-- These options will be passed to conform.format()
+				timeout_ms = 500,
+				lsp_fallback = true,
+			},
+		})
+	end,
+}
