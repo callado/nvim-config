8ba37a88fcfe56bbe00af4f126fe2d0640a187b2
Pedro Callado <pedrocallado@gmail.com>
2023-10-22 00:11:37 +0200

    removes manual transparency

--- lua/callado/plugins/rose-pine.lua
+++ lua/callado/plugins/rose-pine.lua
@@ -62,8 +62,8 @@ return {
 
 		vim.cmd('colorscheme rose-pine')
 
-		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-		vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+		-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+		-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 		-- vim.cmd [[colorscheme rose-pine]]
 	end,
 }
