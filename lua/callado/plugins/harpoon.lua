a9c8f6a4ce3ec433df880a5188520ab294118735
Pedro Callado <pedrocallado@gmail.com>
2023-10-22 11:56:29 +0200

    adds mini pairs, configures lsp

--- lua/callado/plugins/harpoon.lua
+++ lua/callado/plugins/harpoon.lua
@@ -1,17 +1,24 @@
 return {
-	'theprimeagen/harpoon',
-	dependencies = { 'nvim-lua/plenary.nvim' },
+	"theprimeagen/harpoon",
+	dependencies = { "nvim-lua/plenary.nvim" },
 	config = function()
-		
 		local mark = require("harpoon.mark")
 		local ui = require("harpoon.ui")
 
 		vim.keymap.set("n", "<leader>a", mark.add_file)
 		vim.keymap.set("n", "<leader>h", ui.toggle_quick_menu)
 
-		vim.keymap.set("n", "<leader>1", function() ui.nav_file(1) end)
-		vim.keymap.set("n", "<leader>2", function() ui.nav_file(2) end)
-		vim.keymap.set("n", "<leader>3", function() ui.nav_file(3) end)
-		vim.keymap.set("n", "<leader>4", function() ui.nav_file(4) end)
+		vim.keymap.set("n", "<leader>1", function()
+			ui.nav_file(1)
+		end)
+		vim.keymap.set("n", "<leader>2", function()
+			ui.nav_file(2)
+		end)
+		vim.keymap.set("n", "<leader>3", function()
+			ui.nav_file(3)
+		end)
+		vim.keymap.set("n", "<leader>4", function()
+			ui.nav_file(4)
+		end)
 	end,
 }
