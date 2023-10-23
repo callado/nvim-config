b130686d437dfa9107ad9e29e086174162b6ad1a
Pedro Callado <pedro.callado@webeindhoven.com>
2023-10-23 10:43:03 +0200

    adds persistence plugin

--- lua/callado/plugins/lualine.lua
+++ lua/callado/plugins/lualine.lua
@@ -3,6 +3,16 @@ return {
 	requires = { "nvim-tree/nvim-web-devicons", opt = true },
 	lazy = false,
 	priority = 800,
+	init = function()
+		vim.g.lualine_laststatus = vim.o.laststatus
+		if vim.fn.argc(-1) > 0 then
+			-- set an empty statusline till lualine loads
+			vim.o.statusline = " "
+		else
+			-- hide the statusline on the starter page
+			vim.o.laststatus = 0
+		end
+	end,
 	config = function()
 		local theme = require("lualine.themes.rose-pine")
 		local modes = {
