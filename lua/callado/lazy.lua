ab6253b2171fd11aa4e1007e8f001b0ecbc8ce7d
Pedro Callado <pedrocallado@gmail.com>
2023-10-24 22:16:37 +0200

    moves plugins to .old folder

--- lua/callado/lazy.lua
+++ lua/callado/lazy.lua
@@ -30,6 +30,7 @@ require("lazy").setup({
 		-- have outdated releases, which may break your Neovim install.
 		version = false, -- always use the latest git commit
 		-- version = "*", -- try installing the latest stable version for plugins that support semver
+		install = { colorscheme = { "rose-pine" } },
 		checker = { enabled = true }, -- automatically check for plugin updates
 		performance = {
 			rtp = {
